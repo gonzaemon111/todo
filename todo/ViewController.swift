49d3348ab495c58ab68ad3cb265cd0b9f70259f7
gomzaemon111 <goki14914@icloud.com>
2018-10-21 00:52:45 +0900

    Initial Commit

---
+++ todo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todo
+//
+//  Created by 櫻庭豪騎 on 2018/10/21.
+//  Copyright © 2018年 Gonzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
